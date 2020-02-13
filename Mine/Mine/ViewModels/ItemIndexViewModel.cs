e1472393ea245a54c5086e5c7a0410ce1c3722ca
Tejas Dandane <tejudandane@gamil.com>
2020-02-12 22:44:47 -0800

    Updated ItemIndexViewModel to be Singleton

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -16,6 +16,28 @@ namespace Mine.ViewModels
     /// </summary>
     public class ItemIndexViewModel : BaseViewModel
     {
+        private static volatile ItemIndexViewModel instance;
+        private static object syncRoot = new object();
+
+        public static ItemIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new ItemIndexViewModel();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
         // The Data set of records
         public ObservableCollection<ItemModel> Dataset { get; set; }
 
